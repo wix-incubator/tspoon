8103ae6c5d213d32526a9e9a75431a8bc310e10d
Gadi Guy <gadig@wix.com>
2015-08-20 14:01:30 +0300

    fixing imports

--- test/e2e.spec.ts
+++ test/e2e.spec.ts
@@ -1,7 +1,10 @@
+/// <reference path="../typings/chai.d.ts" />
+/// <reference path="../typings/mocha.d.ts" />
+
 import { expect } from 'chai';
 import chai from 'chai';
 import matchers from '../test-kit/matchers';
-
+import { describe } from "mocha";
 
 chai.use(matchers);
 
