dd225637f0b1a6c01a146767293c4723ddc56945
amir-arad <amira@wix.com>
2015-08-25 10:07:23 +0300

    tests are working

--- test/visitors/typoramaVisitor.spec.ts
+++ test/visitors/typoramaVisitor.spec.ts
@@ -2,20 +2,18 @@
 /// <reference path="../../typings/mocha.d.ts" />
 /// <reference path="../../typings/sinon.d.ts" />
 /// <reference path="../../typings/sinon-chai.d.ts" />
-/// <reference path="../../test-kit/matchers.d.ts" />
 /// <reference path="../../src/visitors/TyporamaVisitor.d.ts" />
 
 import { expect, use } from 'chai';
-import matchers from "../../test-kit/matchers";
+import * as chai from 'chai';
 import { tsToAst } from "../../test-kit/index";
 import { Visitor, VisitContext } from "../../src/Visitor";
 import { TyporamaVisitor } from "../../src/visitors/TyporamaVisitor";
 import * as ts from "typescript";
 import * as sinon from "sinon";
-import { sinonChai } from 'sinon-chai';
+import * as sinonChai from 'sinon-chai';
 
 chai.use(sinonChai);
-chai.use(matchers);
 
 describe("typorama visitor", function() {
     it("doesn't touch class that doesn't extend typorama.BaseType", function() {
