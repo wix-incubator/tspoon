a04bc5a445b6f8d931df30a04bc1eed47246440b
Gadi Guy <gadig@wix.com>
2015-08-25 11:23:34 +0300

    merge

---
+++ test/visitors/typoramaVisitor.spec.d.ts
@@ -0,0 +1,4 @@
+/// <reference path="../../typings/chai.d.ts" />
+/// <reference path="../../typings/mocha.d.ts" />
+/// <reference path="../../typings/jasmine.d.ts" />
+/// <reference path="../../src/visitors/TyporamaVisitor.d.ts" />
