3324fc7240205f33e2bc223236c9bd5c76bcf818
Gadi Guy <gadig@wix.com>
2015-08-25 11:26:44 +0300

    Merge remote-tracking branch 'origin/master'

--- test/visitors/typoramaVisitor.spec.d.ts
+++
@@ -1,4 +0,0 @@
-/// <reference path="../../typings/chai.d.ts" />
-/// <reference path="../../typings/mocha.d.ts" />
-/// <reference path="../../typings/jasmine.d.ts" />
-/// <reference path="../../src/visitors/TyporamaVisitor.d.ts" />
