b81a310d50ad88f239541d4c22cd670fa7b90cb0
Gadi Guy <gadig@wix.com>
2015-10-12 15:44:43 +0300

    initial

--- test-kit/index.ts
+++ test-kit/index.ts
@@ -1,26 +1,18 @@
 /// <reference path="../test-kit/matchers/matchers.d.ts" />
 
 import * as chai from 'chai';
-import { evaluateModuleExports, transpileAndEvaluate } from './evaluate-module';
 import syntaxKindMap from "./syntax-kind-map";
 import matchCode from './matchers/match-code';
 import transpileTo from './matchers/transpile-to';
 import { findCodePosition, findCodeRange } from './code-positions';
-import passTypecheck from './matchers/pass-typecheck';
-import defined from './matchers/defined';
 import applyVisitor from "./apply-visitor";
 
 chai.use(matchCode);
 chai.use(transpileTo);
-chai.use(passTypecheck);
-chai.use(defined);
 
 export {
-    evaluateModuleExports,
-    transpileAndEvaluate,
     findCodePosition,
     findCodeRange,
     syntaxKindMap,
     applyVisitor,
-    passTypecheck
 };
