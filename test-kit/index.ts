7c9d54ea1f8f791f3b6ac9f104e9135a531a0b55
Gadi Guy <gadig@wix.com>
2015-10-15 14:43:49 +0300

    fixing stuff

--- test-kit/index.ts
+++ test-kit/index.ts
@@ -1,18 +1,10 @@
-/// <reference path="../test-kit/matchers/matchers.d.ts" />
-
-import * as chai from 'chai';
 import syntaxKindMap from "./syntax-kind-map";
-import matchCode from './matchers/match-code';
-import transpileTo from './matchers/transpile-to';
 import { findCodePosition, findCodeRange } from './code-positions';
 import applyVisitor from "./apply-visitor";
-
-chai.use(matchCode);
-chai.use(transpileTo);
 
 export {
     findCodePosition,
     findCodeRange,
     syntaxKindMap,
-    applyVisitor,
+    applyVisitor
 };
