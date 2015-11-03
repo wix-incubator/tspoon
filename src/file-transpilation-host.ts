0a6867feec94c5c016beb1aba126462dcd80e6a8
tobisek <jiri@wix.com>
2015-11-03 15:18:21 +0200

    cleanup & refactoring

--- src/file-transpilation-host.ts
+++ src/file-transpilation-host.ts
@@ -11,12 +11,54 @@ function fileExtensionIs(path: string, extension: string): boolean {
 	return pathLen > extLen && path.substr(pathLen - extLen, extLen) === extension;
 }
 
-export class FileValidationHost implements ts.CompilerHost {
+class HostBase implements ts.CompilerHost {
+	fileExists(fileName: string): boolean{
+		return false;
+	}
+
+	readFile(fileName: string): string{
+		return null;
+	}
+
+	getSourceFile(fileName: string): ts.SourceFile {
+		return null;
+	}
+
+	writeFile(name:string, text:string, writeByteOrderMark: boolean) {}
+
+	useCaseSensitiveFileNames() {
+		return false;
+	}
+
+	getCanonicalFileName(fileName: string) {
+		return fileName;
+	}
+
+	getCurrentDirectory(): string {
+		return "";
+	}
+
+	getNewLine(): string {
+		return "\n";
+	}
+
+	getDefaultLibFileName(options:ts.CompilerOptions): string {
+		return "lib.d.ts";
+	}
+
+	getCancellationToken(): ts.CancellationToken {
+		return null;
+	}
+}
+
+export class FileValidationHost extends HostBase implements ts.CompilerHost {
 	constructor(
 		private _ast: ts.SourceFile,
 		private _resolutionHosts: ts.ModuleResolutionHost[],
 		private _compilerOptions: ts.CompilerOptions
-	) {}
+	) {
+		super();
+	}
 
 	fileExists(fileName: string): boolean{
 		return fileName === this._ast.fileName || this._resolutionHosts.some(host => host.fileExists(fileName));
@@ -46,39 +88,14 @@ export class FileValidationHost implements ts.CompilerHost {
 			}
 		}
 	}
-
-	writeFile(name:string, text:string, writeByteOrderMark: boolean) {}
-
-	useCaseSensitiveFileNames() {
-		return false;
-	}
-
-	getCanonicalFileName(fileName: string) {
-		return fileName;
-	}
-
-	getCurrentDirectory() {
-		return "";
-	}
-
-	getNewLine() {
-		return "\n";
-	}
-
-	getDefaultLibFileName(options:ts.CompilerOptions): string {
-		return "lib.d.ts";
-	}
-
-	getCancellationToken(): ts.CancellationToken {
-		return null;
-	}
 }
 
-export class FileTranspilationHost implements ts.CompilerHost {
+export class FileTranspilationHost extends HostBase implements ts.CompilerHost {
 	private _output: string = 'NOT_SET';
 	private _map: string = 'NOT_SET';
 
 	constructor(private _ast: ts.SourceFile) {
+		super();
 	}
 
 	public get output(): string {
@@ -111,29 +128,5 @@ export class FileTranspilationHost implements ts.CompilerHost {
 		} else {
 			this._output = text;
 		}
-	}
-
-	useCaseSensitiveFileNames() {
-		return false;
-	}
-
-	getCanonicalFileName(fileName: string) {
-		return fileName;
-	}
-
-	getCurrentDirectory() {
-		return "";
-	}
-
-	getNewLine() {
-		return "\n";
-	}
-
-	getDefaultLibFileName(options:ts.CompilerOptions): string {
-		return "lib.d.ts";
-	}
-
-	getCancellationToken(): ts.CancellationToken {
-		return null;
 	}
 }
--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,7 @@
 /// <reference path="../typings/source-map/source-map.d.ts"/>
 /// <reference path="../typings/node/node.d.ts" />
 
-import { transpile, TranspilerOutput, TranspilerConfig, ValidatorConfig, parse, validate } from './transpile';
-import { RawSourceMap } from 'source-map';
-import { Visitor, VisitorContext } from "./visitor";
-import { applyVisitor } from "./apply-visitor";
+export { transpile, TranspilerOutput, TranspilerConfig, ValidatorConfig, parse, validate } from './transpile';
+export { Visitor, VisitorContext } from "./visitor";
+export { applyVisitor } from "./apply-visitor";
 
-export {
-	TranspilerOutput,
-	TranspilerConfig,
-	ValidatorConfig,
-	Visitor,
-	VisitorContext,
-	parse,
-	validate,
-	transpile,
-	applyVisitor
-};
