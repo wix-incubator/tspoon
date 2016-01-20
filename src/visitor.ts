f359dd7e9e8471eb5b19623a8852bf4a8a3e6d80
amir-arad <amira@wix.com>
2016-01-20 17:28:15 +0200

    documentation first pass done

--- src/visitor.ts
+++ src/visitor.ts
@@ -1,13 +1,49 @@
 import * as ts from 'typescript';
 
+/**
+ * a transpilation actions API supplied to Visitor at visit time
+ */
 export interface VisitorContext {
+	/**
+	 * was the transpilation declared as failed by any previous visitor
+	 */
 	halted: boolean;
+	/**
+	 * add a text line at given position
+	 * @param position position in code at which to insert line.
+	 * @param str line to insert
+	 */
 	insertLine(position: number, str: string): void;
+	/**
+	 * replace a piece of text in the code with a given new text
+	 * @param start position in code of the first character to replace
+	 * @param end position in code of the last character to replace. has to be greater or equal to the start param.
+	 * @param str replacement string to insert instead of original state
+	 */
 	replace(start: number, end: number, str: string): void;
+	/**
+	 * report transpilation diagnostics
+	 * @param node typescript AST node that relats to this diagnostic
+	 * @param category severity of the diagnostic to report (could be error, warning or message)
+	 * @param message txt of the diagnostic
+	 * @param halt if true, the transpilation is declared as failed
+	 */
 	reportDiag(node: ts.Node, category: ts.DiagnosticCategory, message: string, halt?: boolean): void;
 }
 
+/**
+ * single unit of transpilation logic
+ */
 export interface Visitor {
+	/**
+	 * predicate defining whether this visitor should operate on this node
+	 * @param node typescript AST node
+	 */
 	filter(node: ts.Node) : boolean;
+	/**
+	 * perform visitor logic on given node
+	 * @param node typescript AST node for which filter(node) returnes true
+	 * @param context transpilation actions handler for the current visit
+	 */
 	visit(node: ts.Node, context: VisitorContext): void;
 }
