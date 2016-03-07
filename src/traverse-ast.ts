058178ad199d656e667e805a71fbf3b0ce4438f9
tobisek <jiri@wix.com>
2016-03-07 11:08:54 +0200

    Implemented nested visitors. Resolves #21.

--- src/traverse-ast.ts
+++ src/traverse-ast.ts
@@ -1,11 +1,20 @@
 import * as ts from 'typescript';
 import { Visitor, VisitorContext } from './visitor';
 
+function descend(node: ts.Node, context: VisitorContext) {
+	return function visit(...visitors: Visitor[]): void {
+		visitors.forEach(visitor => {
+			traverseAst(node, visitor, context);
+		});
+	}
+}
+
+
 export function traverseAst(root: ts.Node, visitor: Visitor, context: VisitorContext): boolean {
 
 	function traverse(node: ts.Node) {
 		if(visitor.filter(node)) {
-			visitor.visit(node, context, function () {});
+			visitor.visit(node, context, descend(node, context));
 			return context.halted || ts.forEachChild(node, traverse);
 		}
 		return ts.forEachChild(node, traverse);
