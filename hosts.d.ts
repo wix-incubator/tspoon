7c9a96b90a955165e96d006c6700e0df922afeae
tobisek <jiri@wix.com>
2016-04-05 19:50:17 +0300

    WIP

--- hosts.d.ts
+++ hosts.d.ts
@@ -31,3 +31,16 @@ export class SemanticHost extends ChainableHost {
 	constructor(files: string[], compilerOptions?: ts.CompilerOptions);
 }
 
+
+
+export class MultipleFilesHost extends HostBase {
+	constructor(resolutionHosts: ts.ModuleResolutionHost[], compilerOptions?: ts.CompilerOptions)
+	getSyntacticErrors(): ts.Diagnostic[];
+}
+
+export class SingleFileHost extends HostBase {
+	constructor(ast: ts.SourceFile);
+	public output: string;
+	public sourceMap: RawSourceMap;
+}
+
