702ffe6f3699f6714fd70d3f6f876d73c7fd6a6e
Gadi Guy <gadig@wix.com>
2016-04-27 17:43:35 +0300

    wip

---
+++ node_modules/gulp-typedoc/test/data/myutil.ts
@@ -0,0 +1,14 @@
+﻿/**
+ * Example of using a .ts file.
+ * Copyright (c) 2014 Spirit IT BV
+ */
+
+/**
+ * Example exported function.
+ * @return Fixed number 6.
+ */
+export function myUtilityFunction(): number {
+	"use strict";
+	console.log("hello my utility");
+	return 6;
+}
---
+++ node_modules/mocha-env-reporter/node_modules/mocha-teamcity-reporter/History.md
@@ -0,0 +1,9 @@
+
+1.0.0 / 2015-09-30
+==================
+
+  * Merge pull request #14 from debitoor/master
+  * add err.stack to details of error
+  * Merge pull request #8 from bdefore/master
+  * Update teamcity.js
+  * Merge pull request #6 from pandell/mocha-phantomjs
