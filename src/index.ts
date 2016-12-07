688c83f1f48c885f604a2dd41dbc0b6bff6b510e
Avi Vahl <avi.vahl@wix.com>
2016-12-08 00:16:12 +0200

    Export all from apply-visitor

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 export {transpile, TranspilerOutput, TranspilerConfig, ValidatorConfig, validateAll} from './transpile';
 export {Visitor, VisitorContext} from './visitor';
-export {applyVisitor, applyVisitorOnAst, applyVisitorOnHostedSource} from './apply-visitor';
 export {traverseAst} from './traverse-ast';
+export * from './apply-visitor';
 export * from './hosts';
 export * from './hosts-base';
+
