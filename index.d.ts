8bde120233c335d788937fba0fbd9c86ef0d6c5a
Avi Vahl <avi.vahl@wix.com>
2016-12-08 09:31:43 +0200

    Cleanups and export hosts Remove junk bundle from root, old doc folder with locally forked theme, gulp Export hosts from main index file instead from hosts.js

--- index.d.ts
+++ index.d.ts
@@ -72,3 +72,42 @@ export interface ValidatorConfig {
 export function validateAll(files: string[], config: ValidatorConfig): ts.Diagnostic[];
 
 export function traverseAst(root: ts.Node, visitor: Visitor, context: VisitorContext): boolean;
+
+export interface HostBase extends ts.CompilerHost { }
+export class HostBase implements HostBase { }
+
+export class ChainableHost extends HostBase {
+    setSource(source: ts.CompilerHost): void;
+}
+
+export function chainHosts(host0: ts.CompilerHost, ...chainableHosts: ChainableHost[]): ts.CompilerHost;
+
+export class AstCacheHost extends ChainableHost {
+    getSourceFile(fileName: string, languageVersion: ts.ScriptTarget, onError?: (message: string) => void): ts.SourceFile;
+}
+
+export class TransformationHost extends ChainableHost {
+    constructor(visitors: Visitor[], languageServiceProvider?: () => ts.LanguageService);
+    getSourceMap(fileName: string): RawSourceMap;
+    translateDiagnostic(diagnostic: ts.Diagnostic): ts.Diagnostic;
+}
+
+export interface SemanticHost extends ts.LanguageServiceHost, ts.CompilerHost, ts.DocumentRegistry {
+    getCancellationToken(): ts.CancellationToken;
+    getNewLine(): string;
+    useCaseSensitiveFileNames();
+}
+export class SemanticHost extends ChainableHost {
+    constructor(files: string[], compilerOptions?: ts.CompilerOptions);
+}
+
+export class MultipleFilesHost extends HostBase {
+    constructor(resolutionHosts: ts.ModuleResolutionHost[], compilerOptions?: ts.CompilerOptions)
+    getSyntacticErrors(): ts.Diagnostic[];
+}
+
+export class SingleFileHost extends HostBase {
+    constructor(ast: ts.SourceFile);
+    public output: string;
+    public sourceMap: RawSourceMap;
+}
